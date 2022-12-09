5af19f7e07ad9462af9a9e0732b5a4587ee2a6e0
m-shehzad455 <shehzuali5@gmail.com>
2022-12-09 22:57:39 +0500

    arr

---
+++ 02Basics/myArrays.js
@@ -0,0 +1,10 @@
+var superHeros = [];
+var heroPower = [];
+var allUsers = [];
+var MLModels = [
+    [123, 123, 123],
+    [],
+];
+superHeros.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "abcd", isActive: true });
---
+++ 02Basics/myArrays.ts
@@ -0,0 +1,21 @@
+const superHeros:string[] = []
+const heroPower: Array<number> = []
+
+
+type User = {
+    name:string
+    isActive:boolean
+}
+
+
+const allUsers: User[] = []
+
+const MLModels: number[][] = [
+    [123,123,123],
+    [],
+]
+
+superHeros.push("spiderman");
+heroPower.push(2);
+
+allUsers.push({name : "abcd", isActive: true});
